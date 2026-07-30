48c1416ea21ba45b4c89efd77eedf7684edd29b3
code-savan <eric.marvelboy@gmail.com>
2026-07-30 14:25:47 +0100

    update

--- app/page.jsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-black">
-      <h1 className="text-6xl md:text-8xl font-bold text-red-600 tracking-tight">
-        ACCESS DENIED
-      </h1>
-    </div>
-  )
-}
